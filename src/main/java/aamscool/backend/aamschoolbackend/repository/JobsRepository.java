567b6973b645fb59606c0cda81c0ef3044b30a0f
ashutosh <yadavashutosh2193@gmail.com>
2025-11-28 12:51:22 +0530

    canged pathe of model class

--- src/main/java/aamscool/backend/aamschoolbackend/repository/JobsRepository.java
+++ src/main/java/aamscool/backend/aamschoolbackend/repository/JobsRepository.java
@@ -16,7 +16,7 @@ public interface JobsRepository extends JpaRepository<JobPosts, Long>{
 //	@Query("SELECT new com.aamschool.aamschoolapp.model.HomePageLinksModel(j.title,j.jobId) FROM JobPost j WHERE j.label = 'Latest Jobs'")
 //	List<HomePageLinksModel> findJobIdandTitleByLabel();
 
-	@Query("SELECT new com.aamschool.aamschoolapp.model.HomePageLinksModel(j.title,j.jobId,j.createdAt) FROM JobPosts j WHERE j.label =:label")
+	@Query("SELECT new aamscool.backend.aamschoolbackend.model.HomePageLinksModel(j.title,j.jobId,j.createdAt) FROM JobPosts j WHERE j.label =:label")
 	List<HomePageLinksModel> getJobIdandTile(@Param("label") String label);
 
 }
