6d94da5959f1bf5e0c4f9214f293fa3ee3217ef6
ashutosh <yadavashutosh2193@gmail.com>
2025-11-28 11:52:43 +0530

    this is bachend

---
+++ src/test/java/aamscool/backend/aamschoolbackend/AamschoolbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package aamscool.backend.aamschoolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AamschoolbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
