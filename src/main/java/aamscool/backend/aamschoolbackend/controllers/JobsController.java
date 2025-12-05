ea1d97faf26dac8426c93e0a1e6d6bbac5e822b9
ashutosh <yadavashutosh2193@gmail.com>
2025-12-05 22:41:40 +0530

    added cors origins

--- src/main/java/aamscool/backend/aamschoolbackend/controllers/JobsController.java
+++ src/main/java/aamscool/backend/aamschoolbackend/controllers/JobsController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,10 @@ import aamscool.backend.aamschoolbackend.service.JobsService;
 
 
 @RestController
+@CrossOrigin(origins = {
+		"http://localhost:3000",
+        "https://aamschool-production.up.railway.app"
+})
 @RequestMapping("/api/jobs")
 public class JobsController {
 
