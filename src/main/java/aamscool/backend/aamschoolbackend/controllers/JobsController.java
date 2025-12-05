5d058c467744dc31a17b2ae591c5baefd95e0cca
ashutosh <yadavashutosh2193@gmail.com>
2025-12-05 22:35:39 +0530

    added api path

--- src/main/java/aamscool/backend/aamschoolbackend/controllers/JobsController.java
+++ src/main/java/aamscool/backend/aamschoolbackend/controllers/JobsController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,7 @@ import aamscool.backend.aamschoolbackend.service.JobsService;
 
 
 @RestController
+@RequestMapping("/api/jobs")
 public class JobsController {
 
 	private final ObjectMapper mapper = new ObjectMapper();
