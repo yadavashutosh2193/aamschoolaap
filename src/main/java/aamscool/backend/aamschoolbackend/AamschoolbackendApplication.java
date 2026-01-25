0377c9d9f7f5aeec13efc86fa8ae1e073e380ded
ashutosh <yadavashutosh2193@gmail.com>
2026-01-25 16:27:17 +0530

    code chnage for AWS server

--- src/main/java/aamscool/backend/aamschoolbackend/AamschoolbackendApplication.java
+++ src/main/java/aamscool/backend/aamschoolbackend/AamschoolbackendApplication.java
@@ -8,6 +8,6 @@ public class AamschoolbackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AamschoolbackendApplication.class, args);
+		
 	}
-
 }
--- src/main/java/aamscool/backend/aamschoolbackend/model/JobPosts.java
+++ src/main/java/aamscool/backend/aamschoolbackend/model/JobPosts.java
@@ -7,8 +7,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "job_post")
 public class JobPosts {
 
 	@Id
